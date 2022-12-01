1adf9de80b40852d60337c984bf980745e0a11b3
Ryan Hagan <ryan@ryanhagan.net>
2022-12-01 16:34:21 -0500

    Added framework for Day 2

---
+++ Day02/Day02.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Day02;
+
+public class Day02
+{
+    public void SolvePart1(string[] data)
+    {
+    }
+
+    public void SolvePart2(string[] data)
+    {
+    }
+}
---
+++ Day02/tests/Day02Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode.Day02;
+using Xunit;
+
+namespace AdventOfCode.Day02.Tests;
+
+public class Day02Tests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
