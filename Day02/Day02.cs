688505b6444aa62d7e4c7ad7ef6b95ef829c642c
Ryan Hagan <ryan@ryanhagan.net>
2022-12-02 14:40:52 -0500

    Solved Day 2

--- Day02/Day02.cs
+++ Day02/Day02.cs
@@ -5,13 +5,94 @@ using AdventOfCode.Common;
 
 namespace AdventOfCode.Day02;
 
+public enum Outcome
+{
+    Lose = 0,
+    Draw = 3,
+    Win = 6
+}
+
 public class Day02
 {
     public void SolvePart1(string[] data)
     {
+        int totalScore = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            string player1Shape = GetShape(data[i][0]);
+            string player2Shape = GetShape(data[i][2]);
+            totalScore += ScoreGame(player1Shape, player2Shape);
+        }
+
+        System.Console.WriteLine($"Total score for all games is {totalScore}");
     }
 
     public void SolvePart2(string[] data)
     {
+        int totalScore = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            string player1Shape = GetShape(data[i][0]);
+            string gameStrategy = GetStrategy(data[i][2]);
+            string player2Shape = DetermineShapeForGameStrategy(
+                                    player1Shape,
+                                    gameStrategy
+                                    );
+            totalScore += ScoreGame(player1Shape, player2Shape);
+        }
+
+        System.Console.WriteLine($"Total score for all ideal games should be {totalScore}");
+    }
+
+    private Dictionary<char, string> decodeShape = new Dictionary<char, string> {
+            { 'A', "Rock" },
+            { 'B', "Paper" },
+            { 'C', "Scissors" },
+            { 'X', "Rock" },
+            { 'Y', "Paper" },
+            { 'Z', "Scissors" }
+        };
+    private Dictionary<string, int> shapeScores = new Dictionary<string, int> {
+            { "Rock", 1 },
+            { "Paper", 2 },
+            { "Scissors", 3 }
+        };
+    private Dictionary<string, Outcome> outcomes = new Dictionary<string, Outcome> {
+            { "RockRock", Outcome.Draw }, { "RockPaper", Outcome.Lose }, { "RockScissors", Outcome.Win },
+            { "PaperRock", Outcome.Win }, { "PaperPaper", Outcome.Draw }, { "PaperScissors", Outcome.Lose },
+            { "ScissorsRock", Outcome.Lose }, { "ScissorsPaper", Outcome.Win }, { "ScissorsScissors", Outcome.Draw }
+        };
+
+    public int ScoreGame(string elfShape, string ryanShape)
+    {
+        return shapeScores[ryanShape] + (int)outcomes[ryanShape + elfShape];
+    }
+
+    private Dictionary<char, Outcome> decodeStrategy = new Dictionary<char, Outcome> {
+            { 'X', Outcome.Lose },
+            { 'Y', Outcome.Draw },
+            { 'Z', Outcome.Win }
+        };
+
+    private Dictionary<string, string> strategies = new Dictionary<string, string> {
+            { "RockWin", "Paper" }, { "RockDraw", "Rock" }, { "RockLose", "Scissors" },
+            { "PaperWin", "Scissors" }, { "PaperDraw", "Paper" }, { "PaperLose", "Rock" },
+            { "ScissorsWin", "Rock" }, { "ScissorsDraw", "Scissors" }, { "ScissorsLose", "Paper" }
+        };
+
+
+    public string GetShape(char shapeCode)
+    {
+        return decodeShape[shapeCode];
+    }
+
+    public string GetStrategy(char strat)
+    {
+        return decodeStrategy[strat].ToString();
+    }
+
+    public string DetermineShapeForGameStrategy(string elfsPlay, string strategy)
+    {
+        return strategies[elfsPlay + strategy];
     }
 }
--- Day02/tests/Day02Tests.cs
+++ Day02/tests/Day02Tests.cs
@@ -10,8 +10,54 @@ namespace AdventOfCode.Day02.Tests;
 public class Day02Tests
 {
     [Fact]
-    public void Test1()
+    public void ScoreGame_WithSampleData_ShouldReturn15()
     {
+        string[] data = "A Y\nB X\nC Z".Split("\n", StringSplitOptions.TrimEntries);
+        Day02 solver = new Day02();
 
+        int totalScore = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            string player1Shape = solver.GetShape(data[i][0]);
+            string player2Shape = solver.GetShape(data[i][2]);
+            totalScore += solver.ScoreGame(player1Shape, player2Shape);
+        }
+
+        Assert.Equal(15, totalScore);
+    }
+
+    [Fact]
+    public void DetermineStrategy_WhenElfChoosesRockAndNeedToDraw_ShouldReturnRock()
+    {
+        string game = "A Y";
+        Day02 solver = new();
+
+        Assert.Equal("Rock",
+                        solver.DetermineShapeForGameStrategy(
+                                solver.GetShape(game[0]),
+                                solver.GetStrategy(game[2])
+                                )
+                    );
+    }
+
+    [Fact]
+    public void ScoreGamePart2_WithSampleData_ShouldReturn12()
+    {
+        string[] data = "A Y\nB X\nC Z".Split("\n", StringSplitOptions.TrimEntries);
+        Day02 solver = new();
+
+        int totalScore = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            string player1Shape = solver.GetShape(data[i][0]);
+            string gameStrategy = solver.GetStrategy(data[i][2]);
+            string player2Shape = solver.DetermineShapeForGameStrategy(
+                                        player1Shape,
+                                        gameStrategy
+                                        );
+            totalScore += solver.ScoreGame(player1Shape, player2Shape);
+        }
+
+        Assert.Equal(12, totalScore);
     }
 }
