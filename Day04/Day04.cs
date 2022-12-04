6f65dd96e89a48a66f24792dd80063946d3a68f7
Ryan Hagan <ryan@ryanhagan.net>
2022-12-04 10:45:45 -0500

    Updated Day 4 solution to use sets instead of if comparisons

--- Day04/Day04.cs
+++ Day04/Day04.cs
@@ -12,8 +12,9 @@ public class Day04
         int totalPairsWithOverlap = 0;
         for (int i = 0; i < data.Length; i++)
         {
-            (string, string) pair = ParseAssignment(data[i]);
-            if (IsFullOverlap(pair.Item1, pair.Item2))
+            (HashSet<int> p1, HashSet<int> p2) pair = ParseAssignment(data[i]);
+
+            if (IsFullOverlap(pair.p1, pair.p2))
                 totalPairsWithOverlap++;
         }
 
@@ -25,49 +26,43 @@ public class Day04
         int totalPairsWithOverlap = 0;
         for (int i = 0; i < data.Length; i++)
         {
-            (string, string) pair = ParseAssignment(data[i]);
-            if (IsPartialOverlap(pair.Item1, pair.Item2))
+            (HashSet<int> p1, HashSet<int> p2) pair = ParseAssignment(data[i]);
+
+            if (IsPartialOverlap(pair.p1, pair.p2))
                 totalPairsWithOverlap++;
         }
 
         System.Console.WriteLine($"The numer of pairs with partial overlap is {totalPairsWithOverlap}");
     }
 
-    public (string, string) ParseAssignment(string input)
-        => (input.Split(',', StringSplitOptions.TrimEntries)[0],
-            input.Split(',', StringSplitOptions.TrimEntries)[1]);
-
-    public bool IsFullOverlap(string pair1, string pair2)
+    public (HashSet<int>, HashSet<int>) ParseAssignment(string input)
     {
-        bool fullyContained = false;
-        int p1Lower = int.Parse(pair1.Split('-', 2, StringSplitOptions.TrimEntries)[0]);
-        int p1Upper = int.Parse(pair1.Split('-', 2, StringSplitOptions.TrimEntries)[1]);
-        int p2Lower = int.Parse(pair2.Split('-', 2, StringSplitOptions.TrimEntries)[0]);
-        int p2Upper = int.Parse(pair2.Split('-', 2, StringSplitOptions.TrimEntries)[1]);
+        string assignment = input.Split(',', StringSplitOptions.TrimEntries)[0];
+        HashSet<int> p1 = Enumerable
+                            .Range(
+                                int.Parse(assignment.Split('-')[0]),
+                                int.Parse(assignment.Split('-')[1])
+                                    - int.Parse(assignment.Split('-')[0])
+                                    + 1
+                                )
+                            .ToHashSet();
 
-        if (p1Lower >= p2Lower && p1Upper <= p2Upper) fullyContained = true;
-        if (p2Lower >= p1Lower && p2Upper <= p1Upper) fullyContained = true;
+        assignment = input.Split(',', StringSplitOptions.TrimEntries)[1];
+        HashSet<int> p2 = Enumerable
+                            .Range(
+                                int.Parse(assignment.Split('-')[0]),
+                                int.Parse(assignment.Split('-')[1])
+                                    - int.Parse(assignment.Split('-')[0])
+                                    + 1
+                                )
+                            .ToHashSet();
 
-        return fullyContained;
+        return (p1, p2);
     }
 
-    public bool IsPartialOverlap(string pair1, string pair2)
-    {
-        bool partialOverlap = false;
-        (int, int) p1 = (int.Parse(pair1.Split('-', 2, StringSplitOptions.TrimEntries)[0]),
-                          int.Parse(pair1.Split('-', 2, StringSplitOptions.TrimEntries)[1]));
-        (int, int) p2 = (int.Parse(pair2.Split('-', 2, StringSplitOptions.TrimEntries)[0]),
-                          int.Parse(pair2.Split('-', 2, StringSplitOptions.TrimEntries)[1]));
+    public bool IsFullOverlap(HashSet<int> p1, HashSet<int> p2)
+        => (p1.IsSubsetOf(p2) || p2.IsSubsetOf(p1));
 
-        if (p1.Item1 > p2.Item1)
-        {
-            var temp = p1;
-            p1 = p2;
-            p2 = temp;
-        }
-
-        if (p1.Item2 >= p2.Item1 || p2.Item1 <= p1.Item2) partialOverlap = true;
-
-        return partialOverlap;
-    }
+    public bool IsPartialOverlap(HashSet<int> p1, HashSet<int> p2)
+        => (p1.Intersect(p2).Count() > 0);
 }
--- Day04/tests/Day04Tests.cs
+++ Day04/tests/Day04Tests.cs
@@ -13,48 +13,62 @@ public class Day04Tests
     public void IsFullyContained_WithNoOverlap_ShouldBeFalse()
     {
         Day04 solver = new();
-        Assert.False(solver.IsFullOverlap("2-4", "6-8"));
+        Assert.False(solver.IsFullOverlap(new HashSet<int> { 2, 3, 4 },
+                                          new HashSet<int> { 6, 7, 8 }
+                                          ));
     }
 
     [Fact]
     public void IsFullyContained_WithLeftOverlap_ShouldBeFalse()
     {
         Day04 solver = new();
-        Assert.False(solver.IsFullOverlap("5-7", "7-9"));
+        Assert.False(solver.IsFullOverlap(new HashSet<int> { 5, 6, 7 },
+                                          new HashSet<int> { 7, 8, 9 }
+                                          ));
     }
 
     [Fact]
     public void IsFullyContained_WithRightOverlap_ShouldBeFalse()
     {
         Day04 solver = new();
-        Assert.False(solver.IsFullOverlap("7-9", "5-7"));
+        Assert.False(solver.IsFullOverlap(new HashSet<int> { 7, 8, 9 },
+                                          new HashSet<int> { 5, 6, 7 }
+                                          ));
     }
 
     [Fact]
     public void IsFullyContained_WithFullOverlap_ShouldBeTrue()
     {
         Day04 solver = new();
-        Assert.True(solver.IsFullOverlap("6-6", "4-6"));
+        Assert.True(solver.IsFullOverlap(new HashSet<int> { 6 },
+                                         new HashSet<int> { 4, 5, 6 }
+                                         ));
     }
 
     [Fact]
     public void IsPartiallyContained_WithNoOverlap_ShouldBeFalse()
     {
         Day04 solver = new();
-        Assert.False(solver.IsPartialOverlap("2-4", "6-8"));
+        Assert.False(solver.IsPartialOverlap(new HashSet<int> { 2, 3, 4 },
+                                             new HashSet<int> { 6, 7, 8 }
+                                             ));
     }
 
     [Fact]
     public void IsPartiallyContained_WitLowerOverlap_ShouldBeTrue()
     {
         Day04 solver = new();
-        Assert.True(solver.IsPartialOverlap("5-7", "7-9"));
+        Assert.True(solver.IsPartialOverlap(new HashSet<int> { 5, 6, 7 },
+                                            new HashSet<int> { 7, 8, 9 }
+                                            ));
     }
 
     [Fact]
     public void IsPartiallyContained_WitUpperOverlap_ShouldBeTrue()
     {
         Day04 solver = new();
-        Assert.True(solver.IsPartialOverlap("6-6", "4-6"));
+        Assert.True(solver.IsPartialOverlap(new HashSet<int> { 6 },
+                                            new HashSet<int> { 4, 5, 6 }
+                                            ));
     }
 }
