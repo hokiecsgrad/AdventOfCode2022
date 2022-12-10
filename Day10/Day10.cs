c11fcb2630389450e7915a8987496346ad2fc61a
Ryan Hagan <ryan@ryanhagan.net>
2022-12-09 20:46:06 -0500

    Day 10 framework created.

---
+++ Day10/Day10.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Day10;
+
+public class Day10
+{
+    public void SolvePart1(string[] data)
+    {
+    }
+
+    public void SolvePart2(string[] data)
+    {
+    }
+}
---
+++ Day10/tests/Day10Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode.Day10;
+using Xunit;
+
+namespace AdventOfCode.Day10.Tests;
+
+public class Day10Tests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
